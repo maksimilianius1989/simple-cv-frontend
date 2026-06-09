317b25004406763d44dc30fbf934b8edd383f0f2
ViMax <it.vimax@gmail.com>
2026-06-09 19:19:05 +0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,28 @@
 async function authFetch(url, options = {}) {
   let token = localStorage.getItem(ACCESS_TOKEN);
 
-  let res = await fetch(url, {
+  let response = await fetch(url, {
     ...options,
     headers: {
-      ...(options.headers || {}),
+      ...options.headers,
       Authorization: `Bearer ${token}`,
     },
   });
 
-  if (res.status == 200) {
-    return res;
+  if (response.ok) {
+    return response;
   }
 
-  if (res.status === 401) {
+  if(response.status !== 401) {
+    throw new Error(`HTTP ${response.status}`);
+  }
+
+ 
     const refreshed = await refreshTokent();
 
     if (!refreshed) {
-      logout();
+       await logout();
+       
       return null;
     }
 
@@ -32,31 +37,49 @@ async function authFetch(url, options = {}) {
     });
 
     return res;
-  }
 
-  throw new Error("Response error", res);
+
+  const errorText = await res.text();
+
+  throw new Error(`Request failed: ${res.status} ${text}`);
 }
 
+let refreshPromise = null;
 async function refreshTokent() {
-  try {
-    const res = await fetch(`${APP_CONFIG.API_URL}/auth/refresh`, {
-      method: "POST",
-      credentials: "include",
-    });
-
-    if (!res.ok) return false;
-
-    const data = await res.json();
-
-    localStorage.setItem(ACCESS_TOKEN, data.accessToken);
-
-    return true;
-  } catch (e) {
-    return false;
+  if (refreshPromise) {
+    return refreshPromise;
   }
+
+  refreshPromise = (async () => {
+    try {
+      const res = await fetch(`${APP_CONFIG.API_URL}/auth/refresh`, {
+        method: "POST",
+        credentials: "include",
+      });
+
+      if (!res.ok) return false;
+
+      const data = await res.json();
+
+      localStorage.setItem(ACCESS_TOKEN, data.accessToken);
+
+      return true;
+    } finally {
+      refreshPromise = null;
+    }
+  })();
+
+  return refreshPromise;
 }
 
 async function logout() {
-  localStorage.removeItem(ACCESS_TOKEN);
-  window.location.href = "/";
+  try {
+    await fetch(`${APP_CONFIG.API_URL}/auth/logout`, {
+      method: "POST",
+      credentials: "include",
+    });
+  } finally {
+    localStorage.removeItem(ACCESS_TOKEN);
+    window.location.href = "/";
+  }
 }
