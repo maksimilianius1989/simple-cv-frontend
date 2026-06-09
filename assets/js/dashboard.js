6e92fb1889d98ddd08bf6b263d8bf9b991344d58
ViMax <it.vimax@gmail.com>
2026-06-09 18:11:02 +0300

    fix date

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -19,18 +19,26 @@ function renderCard(cvs) {
   const container = document.getElementById("resume-container");
 
   cvs.forEach((cv) => {
+    const updatedAt = new Date(cv.updatedAt).toLocaleString("uk-UA", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+
     const card = document.createElement("div");
     card.classList.add("resume-card");
     card.innerHTML = `
     <div class="resume-preview"></div>
     <div class="resume-content">
       <h3>${cv.title}</h3>
-      <span>${cv.updatedAt}</span>
+      <span>${updatedAt}</span>
     </div>
 
     <div class="resume-actions">
       <button><i class="fa-solid fa-eye"></i></button>
-      <button><i class="fa-solid fa-pen"></i></button>
+      <button><i class="fa-solid fa-file-pdf"></i></button>
       <button><i class="fa-solid fa-trash"></i></button>
     </div>`;
 
