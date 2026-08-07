463c1bb147a20a72bb5c33efcbed97d61c746917
ViMax <it.vimax@gmail.com>
2026-08-07 18:39:06 +0300

    rendering random tempalte

--- parts/templates/template-container.js
+++ parts/templates/template-container.js
@@ -1,13 +1,13 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  TemplateContainer.init();
+document.addEventListener("DOMContentLoaded", async (event) => {
+  await TemplateContainer.init();
 });
 
 class TemplateContainer {
-  static init() {
+  static async init() {
     const container = document.getElementById("templates-list");
     if (!container) return;
 
-    TemplateContainer.render(container);
+    await TemplateContainer.render(container);
   }
 
   static async render(container) {
@@ -21,17 +21,12 @@ class TemplateContainer {
     if (!htmlTemplate) return;
 
     container.innerHTML = "";
-    templates.forEach((template) => {
+    templates.forEach(async (template) => {
       const clone = htmlTemplate.content.cloneNode(true);
 
-      const cardLink = clone.querySelector(".template-card");
       const title = clone.querySelector("h3");
       const category = clone.querySelector(".template-badge");
       const iframe = clone.querySelector("iframe");
-
-      if (cardLink) {
-        cardLink.href = `${APP_CONFIG.API_URL}/templates/${template.id}/render`;
-      }
 
       if (title && template.name) {
         title.textContent = template.name;
@@ -40,12 +35,29 @@ class TemplateContainer {
       if (category && template.category) {
         category.textContent = template.category;
         console.log(template.category);
-        
       }
 
-      if(iframe) {
-        iframe.src = `${APP_CONFIG.API_URL}/templates/${template.id}/render`;
-        iframe.title = template.name || 'Template';
+      if (iframe) {
+        try {
+          const cvContentResponse = await fetch(`${APP_CONFIG.API_URL}/templates/random-content`);
+          if (!cvContentResponse.ok) throw new Error(`Failed to uplaod content ${template.id}`);
+          const cvContent = await cvContentResponse.json();
+
+          const templateResponse = await fetch(`${APP_CONFIG.API_URL}/templates/${template.id}/render`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(cvContent),
+          });
+
+          if (!templateResponse.ok) throw new Error(`Failed to uplaod tempalte ${template.id}`);
+
+          const htmlContent = await templateResponse.text();
+          iframe.srcdoc = htmlContent;
+        } catch (e) {
+          console.error(e)
+        }
       }
 
       container.appendChild(clone);
